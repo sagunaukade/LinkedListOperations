976e9e48c7dbd1f86ce7531daafcc7a1566d9090
sagunaukade <sagunaukade96@gmail.com>
2022-02-03 17:27:03 +0530

    Simple linked list operations

--- LinkedListOperations/LinkedListOperations/Program.cs
+++ LinkedListOperations/LinkedListOperations/Program.cs
@@ -11,7 +11,17 @@ namespace LinkedListOperations
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Linked List Operations");
+            //UseCase 1
+            Operation obj = new Operation();
+            //Insertion Operation
+            obj.InsertLast(56);
+            obj.InsertLast(30);
+            obj.InsertLast(70);
+            //Display Operation
+            Console.WriteLine("Insertion at Last");
+            obj.Display();
             Console.ReadLine();
         }
+
     }
 }
