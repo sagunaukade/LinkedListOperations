376258196420a8fa2cf52201a30449f6d62b7ac9
sagunaukade <sagunaukade96@gmail.com>
2022-02-03 17:19:15 +0530

    welcome Message

---
+++ LinkedListOperations/LinkedListOperations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Linked List Operations");
+            Console.ReadLine();
+        }
+    }
+}
