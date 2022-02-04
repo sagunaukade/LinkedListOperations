0d01f11c3bdd98b5222797ce5d99e7aa7c8ad17c
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-04 10:26:53 +0530

    Adding program to insersion at front

---
+++ LinkedListOperations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Linked List Operations");
+            //UseCase 1
+            Operation obj = new Operation();
+            //Insertion Operation
+            obj.InsertFront(56);
+            obj.InsertFront(30);
+            obj.InsertFront(70);
+            //Display Operation
+            Console.WriteLine("Insertion at First");
+            obj.Display();
+            Console.ReadLine();
+
+
+        }
+    }
+}
